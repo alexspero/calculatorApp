eeb11024e6700fa8e0c172d1442387dd225745d3
alexspero <alexander.m.spero@vanderbilt.edu>
2023-06-07 20:55:05 -0400

    First version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,116 +1,155 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
   const [num, setNum] = useState("0");
-  const [tempNum, setTempNum] = useState(0);
+  const [operator, setOperator] = useState("");
+  const [useTempNum, setUseTempNum] = useState(false);
+  const [tempNum, setTempNum] = useState("");
 
-  const handleOperatorClick = () => {
-
+  const handleOperatorClick = (operator) => {
+    setUseTempNum(true);
+    setOperator(operator);
   }
 
-  const test = 1;
+  const handleSecondNum = (newNum) => {
+    setTempNum(tempNum + newNum);
+  }
+
+  const handleClear = () => {
+    setNum("0");
+    setTempNum("");
+    setUseTempNum(false);
+    setOperator("");
+  }
+
+  const handleOperation = () => {
+    if (operator === '+') {
+      setNum(Number(((parseFloat(num) + parseFloat(tempNum))).toLocaleString('fullwide', {maximumSignificantDigits: 6})));
+    }
+    else if (operator === '-') {
+      setNum(Number(((parseFloat(num) - parseFloat(tempNum))).toLocaleString('fullwide', {maximumSignificantDigits: 6})));
+    }
+    else if (operator === 'x') {
+      setNum(Number(((parseFloat(num) * parseFloat(tempNum))).toLocaleString('fullwide', {maximumSignificantDigits: 6})));
+    }
+    else {
+      if (tempNum === '0') {
+        setNum("Error");
+      }
+      else {
+        setNum(Number(((parseFloat(num) / parseFloat(tempNum))).toLocaleString('fullwide', {maximumSignificantDigits: 6})));
+      }
+    }
+    setUseTempNum(false);
+    setTempNum("");
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <h1>
-            {num}
-          </h1>
-          <h2>
-            {Number(parseFloat(test.toString())).toFixed(2)}
-          </h2>
-        </div>
-        <div>
-          <button
-            className='specialButton'
-            onClick={() => setNum("0")}>
-            AC
-          </button>
-          <button
-            className='specialButton'
-            onClick={() => setNum((Number(num) * -1)).toString()}>
-            +/-
-          </button>
-          <button
-            className='specialButton'
-            onClick={() => setNum((num / 100).toString())}>
-            %
-          </button>
-          <button 
-          className='operatorButton'
-          onClick={handleOperatorClick}
-          >
-            /
-          </button>
-        </div>
-        <div>
-          <button 
-          className='numberButton'
-          onClick={() => setNum(num === "0" ? "7" : num + "7")}>
-            7
-          </button>
-          <button className='numberButton'
-          onClick={() => setNum(num === "0" ? "8" : num + "8")}>
-            8
-          </button>
-          <button className='numberButton'
-          onClick={() => setNum(num === "0" ? "9" : num + "9")}>
-            9
-          </button>
-          <button className='operatorButton'>
-            x
-          </button>
-        </div>
-        <div>
-          <button className='numberButton'
-          onClick={() => setNum(num === "0" ? "4" : num + "4")}>
-            4
-          </button>
-          <button className='numberButton'
-          onClick={() => setNum(num === "0" ? "5" : num + "5")}>
-            5
-          </button>
-          <button className='numberButton'
-          onClick={() => setNum(num === "0" ? "6" : num + "6")}>
-            6
-          </button>
-          <button className='operatorButton'>
-            -
-          </button>
-        </div>
-        <div>
-          <button className='numberButton'
-          onClick={() => setNum(num === "0" ? "1" : num + "1")}>
-            1
-          </button>
-          <button className='numberButton'
-          onClick={() => setNum(num === "0" ? "2" : num + "2")}>
-            2
-          </button>
-          <button className='numberButton'
-          onClick={() => setNum(num === "0" ? "3" : num + "3")}>
-            3
-          </button>
-          <button className='operatorButton'>
-            +
-          </button>
-        </div>
-        <div>
-          <button className='zeroButton'
-          onClick={() => setNum(num === "0" ? "0" : num + "0")}>
-            0
-          </button>
-          <button className='numberButton'
-          onClick={() => setNum(num + "..")}>
-            .
-          </button>
-          <button className='operatorButton'>
-            =
-          </button>
+        <p>
+          Calculator
+        </p>
+        <div className="calculator-overall">
+          <div>
+            <h3 className='number'>
+              {useTempNum && tempNum !== '' ? tempNum : num}
+            </h3>
+          </div>
+          <div>
+            <button
+              className='special-button'
+              onClick={handleClear}>
+              AC
+            </button>
+            <button
+              className='special-button'
+              onClick={() => useTempNum ? setTempNum((Number(tempNum) * -1).toString()) : setNum((Number(num) * -1).toString())}>
+              +/-
+            </button>
+            <button
+              className='special-button'
+              onClick={() => useTempNum ? setTempNum((num / 100).toString()) : setNum((num / 100).toString())}>
+              %
+            </button>
+            <button
+              className='operator-button'
+              onClick={() => handleOperatorClick('/')}
+            >
+              /
+            </button>
+          </div>
+          <div>
+            <button
+              className='number-button'
+              onClick={() => useTempNum ? handleSecondNum("7") : setNum(num === "0" ? "7" : num + "7")}>
+              7
+            </button>
+            <button className='number-button'
+              onClick={() => useTempNum ? handleSecondNum("8") : setNum(num === "0" ? "8" : num + "8")}>
+              8
+            </button>
+            <button className='number-button'
+              onClick={() => useTempNum ? handleSecondNum("9") : setNum(num === "0" ? "9" : num + "9")}>
+              9
+            </button>
+            <button className='operator-button'
+              onClick={() => handleOperatorClick('x')}>
+              x
+            </button>
+          </div>
+          <div>
+            <button className='number-button'
+              onClick={() => useTempNum ? handleSecondNum("4") : setNum(num === "0" ? "4" : num + "4")}>
+              4
+            </button>
+            <button className='number-button'
+              onClick={() => useTempNum ? handleSecondNum("5") : setNum(num === "0" ? "5" : num + "5")}>
+              5
+            </button>
+            <button className='number-button'
+              onClick={() => useTempNum ? handleSecondNum("6") : setNum(num === "0" ? "6" : num + "6")}>
+              6
+            </button>
+            <button className='operator-button'
+              onClick={() => handleOperatorClick('-')}>
+              -
+            </button>
+          </div>
+          <div>
+            <button className='number-button'
+              onClick={() => useTempNum ? handleSecondNum("1") : setNum(num === "0" ? "1" : num + "1")}>
+              1
+            </button>
+            <button className='number-button'
+              onClick={() => useTempNum ? handleSecondNum("2") : setNum(num === "0" ? "2" : num + "2")}>
+              2
+            </button>
+            <button className='number-button'
+              onClick={() => useTempNum ? handleSecondNum("3") : setNum(num === "0" ? "3" : num + "3")}>
+              3
+            </button>
+            <button className='operator-button'
+              onClick={() => handleOperatorClick('+')}>
+              +
+            </button>
+          </div>
+          <div>
+            <button className='zero-button'
+              onClick={() => useTempNum ? handleSecondNum("0") : setNum(num === "0" ? "0" : num + "0")}>
+              0
+            </button>
+            <button className='number-button'
+              onClick={() => useTempNum ? handleSecondNum(".") : setNum(num.includes('.') ? num : num + ".")}>
+              .
+            </button>
+            <button className='operator-button'
+              onClick={handleOperation}>
+              =
+            </button>
+          </div>
         </div>
       </header>
     </div>
